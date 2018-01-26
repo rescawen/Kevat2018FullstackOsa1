df0624d9c8ba672ac47141335df1d073432427b2
rescawen <resca.wen@gmail.com>
2018-01-26 11:47:37 +0200

    1.1 tehty

--- tehtavat/src/index.js
+++ tehtavat/src/index.js
@@ -9,6 +9,21 @@ const Otsikko = (props) => {
     )
   }
 
+  const Sisalto = (props) => {
+    return (
+      <div>
+        <p>{props.osa} {props.tehtavia}</p>
+      </div>
+    )
+  }
+  const Yhteensa = (props) => {
+    return (
+      <div>
+        <p>yhteensä {props.yhteensa} tehtävää</p>
+      </div>
+    )
+  }
+
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
   const osa1 = 'Reactin perusteet'
@@ -21,11 +36,10 @@ const App = () => {
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      
-      <p>{osa1} {tehtavia1}</p>
-      <p>{osa2} {tehtavia2}</p>
-      <p>{osa3} {tehtavia3}</p>
-      <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+      <Sisalto osa ={osa1} tehtavia = {tehtavia1}/>
+      <Sisalto osa ={osa2} tehtavia = {tehtavia2}/>
+      <Sisalto osa ={osa3} tehtavia = {tehtavia3}/>
+      <Yhteensa yhteensa = {tehtavia1 + tehtavia2 + tehtavia3}/>
     </div>
   )
 }
