ab4db3f53bfb9e3ed8a33f03fc6a43cd931e7e4b
rescawen <resca.wen@gmail.com>
2018-01-26 12:12:41 +0200

    1.1 fixed and 1.2 done

--- tehtavat/src/index.js
+++ tehtavat/src/index.js
@@ -3,48 +3,58 @@ import ReactDOM from 'react-dom'
 
 const Otsikko = (props) => {
     return (
-      <div>
-        <h1>{props.kurssi}</h1>
-      </div>
+        <div>
+            <h1>{props.kurssi}</h1>
+        </div>
     )
-  }
+}
 
-  const Sisalto = (props) => {
+const Osa = (props) => {
     return (
-      <div>
-        <p>{props.osa} {props.tehtavia}</p>
-      </div>
+        <div>
+            <p>{props.osa} {props.tehtavia}</p>
+        </div>
     )
-  }
-  const Yhteensa = (props) => {
+}
+
+const Sisalto = (props) => {
     return (
-      <div>
-        <p>yhteensä {props.yhteensa} tehtävää</p>
-      </div>
+        <div>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
+            <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
+        </div>
     )
-  }
+}
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.yhteensa} tehtävää</p>
+        </div>
+    )
+}
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = 'Reactin perusteet'
+    const tehtavia1 = 10
+    const osa2 = 'Tiedonvälitys propseilla'
+    const tehtavia2 = 7
+    const osa3 = 'Komponenttien tila'
+    const tehtavia3 = 14
 
-  return (
-    <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto osa ={osa1} tehtavia = {tehtavia1}/>
-      <Sisalto osa ={osa2} tehtavia = {tehtavia2}/>
-      <Sisalto osa ={osa3} tehtavia = {tehtavia3}/>
-      <Yhteensa yhteensa = {tehtavia1 + tehtavia2 + tehtavia3}/>
-    </div>
-  )
+    return (
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto osa1={osa1} tehtavia1={tehtavia1}
+                osa2={osa2} tehtavia2={tehtavia2}
+                osa3={osa3} tehtavia3={tehtavia3} />
+            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3} />
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
