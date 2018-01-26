0164a34f387c65f45a9996a97288705a8a85dc7c
rescawen <resca.wen@gmail.com>
2018-01-26 10:49:24 +0200

    juuri aloitetaan osa1 tehtavat

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
 
+const Hello = (props) => {
+    return (
+      <div>
+        <p>Hello {props.name}, you are {props.age} years old</p>
+      </div>
+    )
+  }
+
+  const Footer = () => {
+    return (
+      <div>greeting app created by <a href="https://github.com/mluukkai">mluukkai</a></div>
+    )
+  }
+  
+  const App = () => {
+    return (
+      <div>
+        <h1>Greetings</h1>
+        <Hello name="Arto" age={26 + 10} />
+        <Footer />
+      </div>
+    )
+  }
+
+  
 ReactDOM.render(<App />, document.getElementById('root'))
